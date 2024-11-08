7f46532f81869b653b3defda77c573cad093db8c
ArthurRozendo <00001093233059sp@al.educacao.sp.gov.br>
2024-11-08 11:46:18 -0300

    Add files via upload

---
+++ minha ferramenta de estudo/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é minecraft?',
+    'minecraft é um jogo de construir e jogar com amigos'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital de portugal?',
+    'A capital de portugal é lisboa'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz meus amigos não conseguem me acompanhar em um jogo em Inglês?',
+    'traduzindo esta frase em ingles é my friends cant keep up with me in a game '
+)
